8c23d8e95c9a11ff9575528f3dd61ad39ca55af2
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2024-09-01 13:50:59 +0700

    Update main_widget.py
    - Remove upload widget
    - Add Mixture-Hack widget
    - Correct signal sending

--- package/ui/main_widget.py
+++ package/ui/main_widget.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QWidget, QTabWidget, QVBoxLayout
-from package.ui.widget_mixture_2 import WidgetMixture
+from package.ui.widget_mixture import WidgetMixture
 from package.ui.widget_calibration import WidgetCalibration
+from package.ui.widget_mixture_hack import WidgetMixtureHack
 
 class MainWidget(QWidget):
     def __init__(self):
@@ -9,19 +10,18 @@ class MainWidget(QWidget):
         self.showMaximized()
 
         tab_widget = QTabWidget()
-        # widget_upload = WidgetUpload()
         widget_calibration = WidgetCalibration()
         widget_mixture = WidgetMixture()
+        widget_mixture_hack = WidgetMixtureHack()
         
-        widget_calibration.data_sent.connect(widget_mixture.on_signal_from_calibration)
-        # widget_upload.data_sent.connect(widget_image_process.recieve_image_path)
-
+        widget_calibration.data_sent.connect(widget_mixture_hack.on_signal_from_calibration)
+        widget_mixture.data_sent.connect(widget_mixture_hack.on_signal_from_mixture)
+        
         # Add tabs to widget
-        # tab_widget.addTab(widget_upload, "Upload")
         tab_widget.addTab(widget_calibration, "Calibration")
         tab_widget.addTab(widget_mixture, "Mixture")
+        tab_widget.addTab(widget_mixture_hack, "Mixture Hack")
 
         layout = QVBoxLayout()
         layout.addWidget(tab_widget)
-
         self.setLayout(layout)
