4ccc672c463e058a5a1031e13284b215460bfd2b
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2024-09-03 20:37:46 +0700

    Update mixture_hack.py
    Create MixtureHack class

--- package/tlc_class/mixture_hack.py
+++ package/tlc_class/mixture_hack.py
@@ -2,31 +2,51 @@ from package.tlc_class.mixture import Mixture
 from package.tlc_class.calibration import Calibration
 import sympy as sp
 
-def calculate_concentration(mixture: Mixture, calibration: list[Calibration]):
-    # Define symbolic variables
-    log = ""
-    variables = [sp.symbols(f'c{i+1}') for i in range(len(calibration))]
-    log += f"There are {len(calibration)} calibration object --> variables are {variables}\n"
-
-    equations = []
-    coef = []
-    expression = []
-    good_peak_index = []
-    for i in range(len(mixture.peak_area['R'])):
-        for calib in calibration:
-            if calib.peaks[i].best_fit_line['R'] is None:
-                continue
-            else:
-                good_peak_index.append(i)
-                coef.append(calib.peaks[i].best_fit_line['R'][0])
-                const = sum([calib.peaks[i].best_fit_line['R'][1]])
-        log += f"Equation {i+1}:\n\tcoef: {coef}\n\tconstant: {const}\n"
-        expression.append(sum(a*v for a,v in zip(coef, variables)) + const)
+class MixtureHack:
+    def __init__(self, mixture: Mixture, list_calibration: list[Calibration]):
+        self.mixture = mixture
+        self.list_calibration = list_calibration
+        self.list_variable = []
+        self.expression = {}
+        self.equation = {}
+        
+        self.__create_variable()
+        self.__create_expression()
+        self.__create_equation()
     
-    for peak_index in good_peak_index:
-        equations.append(sp.Eq(mixture.peak_area['R'][peak_index], expression))
-        log += f'\t{equations[i]}'
+    def solve_equation(self, list_peak_index: list[int], color: str):
+        print("\nSolving equation system...")
+        list_equation = [self.equation[peak_index][color] for peak_index in list_peak_index]
+        text = f"Equation: {list_equation}\nSolution: {sp.solve(list_equation, self.list_variable)}\n\n"
+        print(text)
+        return text
     
-    solution = sp.solve(equations, variables)
-    log += f"Solution is {solution}"
-    return log
+    # TODO
+    def plot_answer(self):
+        pass
+    
+    def __create_variable(self):
+        print("\nCreating variable...")
+        self.list_variable = [sp.symbols(f'c{i+1}') for i in range(len(self.list_calibration))]
+        print(f'Calibration Count: {len(self.list_calibration)}\nVariables: {self.list_variable}')
+    
+    def __create_expression(self):
+        print("\nCreating expression...")
+        for peak_index in range(len(self.mixture.peak_area['R'])):
+            expression = {}
+            for color in 'RGB':
+                coef = [calibration.peaks[peak_index].best_fit_line[color][0] for calibration in self.list_calibration]
+                constant = sum([calibration.peaks[peak_index].best_fit_line[color][1] for calibration in self.list_calibration])
+                expression[color] = sum(a*v for a,v in zip(coef, self.list_variable)) + constant
+            self.expression[peak_index+1] = expression
+            print(f'Expression {peak_index+1}: {self.expression[peak_index+1]}')
+    
+    def __create_equation(self):
+        print("\nCreating equation...")
+        for peak_index in range(len(self.mixture.peak_area['R'])):
+            equation = {}
+            for color in 'RGB':
+                peak_area = self.mixture.peak_area[color][peak_index]
+                equation[color] = sp.Eq(peak_area, self.expression[peak_index+1][color])
+            self.equation[peak_index+1] = equation
+            print(f'Equation {peak_index+1}: {self.equation[peak_index+1]}')
