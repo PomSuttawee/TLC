e96dd216691d51563844af45c264dfa602d03e17
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2024-09-08 14:13:12 +0700

    Update widget_mixture_hack.py

--- package/ui/widget_mixture_hack.py
+++ package/ui/widget_mixture_hack.py
@@ -27,15 +27,15 @@ class WidgetMixtureHack(QWidget):
         self.init_middle_layout()
         self.init_right_layout()
         main_layout.addLayout(self.v_layout_left, 1)
-        main_layout.addLayout(self.v_layout_middle, 2)
-        main_layout.addLayout(self.v_layout_right, 2)
+        main_layout.addLayout(self.v_layout_middle, 1)
+        main_layout.addLayout(self.v_layout_right, 3)
         self.setLayout(main_layout)
     
     def init_left_layout(self):
         self.v_layout_left = QVBoxLayout()
         ## Button
         button_hack = QPushButton("Calculate Concentration")
-        button_hack.clicked.connect(self.hack)
+        button_hack.clicked.connect(self.calculate_concentration)
         ## Mixture List Widget
         self.list_widget_mixture_object = QListWidget()
         ## Calibration List Widget
@@ -52,10 +52,8 @@ class WidgetMixtureHack(QWidget):
         
     def init_right_layout(self):
         self.v_layout_right = QVBoxLayout()
-        self.label_answer = QLabel()
-        self.v_layout_right.addWidget(self.label_answer)
     
-    def hack(self):
+    def calculate_concentration(self):
         selected_mixture_name = self.list_widget_mixture_object.currentItem().text()
         selected_calibration_name = [item.text() for item in self.list_widget_calibration_object.selectedItems()]
         
@@ -63,25 +61,14 @@ class WidgetMixtureHack(QWidget):
         list_selected_calibration_object = [self.dict_calibration_object[name] for name in selected_calibration_name]
         
         mh = MixtureHack(selected_mixture_object, list_selected_calibration_object)
-        text_answer = ''
-        for i, color in enumerate('RGB'):
-            text_answer += mh.solve_equation(list_peak_index=[1], color=color)
+        log = ''
+        for color in 'RGB':
+            log += f'============ {color} Channel ============\n'
+            log += mh.solve_equation(color=color)
 
-        self.label_answer.setText(text_answer)
-        # mh.solve_equation([1, 2], 'R')
-        # mh.solve_equation([1, 2], 'G')
-        # mh.solve_equation([1, 2], 'B')
-        # mh.solve_equation([1, 2, 3], 'R')
-        # mh.solve_equation([1, 2, 3], 'G')
-        # mh.solve_equation([1, 2, 3], 'B')
+        self.label_hack_data.setText(log)
         
-        text_data = f'Mixture: {selected_mixture_name}\n\tPeak Area: {selected_mixture_object.peak_area}'
-        for calibration_object in list_selected_calibration_object:
-            text_data += f'\n\nCalibration: {calibration_object.name}'
-            for i, peak in enumerate(calibration_object.peaks):
-                text_data += f'\n\tPeak {i+1}:\n\t\tBest fit line: {peak.best_fit_line}\n\t\tR2: {peak.r2}'
-            
-        self.label_hack_data.setText(text_data)
+        
     
     # SIGNAL
     def on_signal_from_calibration(self, dict_calibration_object: dict[str, Calibration]):
